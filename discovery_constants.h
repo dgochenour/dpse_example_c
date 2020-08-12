2d4a51dcf9a211e608c0047f0bbf4efc42024b64
Don Gochenour <don@rti.com>
2020-08-11 20:47:31 -0400

    initial changes for 2nd subscriber application

--- discovery_constants.h
+++ discovery_constants.h
@@ -2,15 +2,15 @@
 #define RTPS_OBJECT_IDS_H
 
 // contants for the publishing application (example_publisher.exe)
-static const char *k_PARTICIANT01_NAME          = "publisher01";
+static const char *k_PARTICIANT01_NAME          = "publisher_1";
 static const int k_OBJ_ID_PARTICIANT01_DW01     = 100;
 
-// contants for one of the subscribing applications (example_subscriber01.exe) 
-static const char *k_PARTICIANT02_NAME          = "subscriber01";
+// contants for one of the subscribing applications (example_subscriber_1.exe) 
+static const char *k_PARTICIANT02_NAME          = "subscriber_1";
 static const int k_OBJ_ID_PARTICIANT02_DR01     = 200;
 
-// contants for the other subscribing application (example_subscriber02.exe)
-static const char *k_PARTICIANT03_NAME          = "subscriber02";
+// contants for the other subscribing application (example_subscriber_2.exe)
+static const char *k_PARTICIANT03_NAME          = "subscriber_2";
 static const int k_OBJ_ID_SUBSCRIBER03_DR01     = 300;
 
 #endif
