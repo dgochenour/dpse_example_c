5fbfe181483a8930db1191cc276adbd5b90bf6b0
Don Gochenour <don@rti.com>
2023-01-31 14:03:27 -0500

    switch default for MacOS dev

--- nic_config.h
+++ nic_config.h
@@ -17,14 +17,14 @@
 int domain_id = 100;
 char *peer = "127.0.0.1";
 
-char *loopback_name = "lo";         // Ubuntu 20.04
-char *eth_nic_name = "wlp0s20f3";   // Ubuntu 20.04    
+// char *loopback_name = "lo";         // Ubuntu 20.04
+// char *eth_nic_name = "wlp0s20f3";   // Ubuntu 20.04    
 
 // char *loopback_name = "Loopback Pseudo-Interface 1";    // Windows 10
 // char *eth_nic_name = "Wireless LAN adapter Wi-Fi";      // Windows 10
 
-// char *loopback_name = "lo0";        // MacOS 10.15.x
-// char *eth_nic_name = "en0";         // MacOS 10.15.x 
+char *loopback_name = "lo0";        // MacOS 10.15.x
+char *eth_nic_name = "en0";         // MacOS 10.15.x 
 
 
 
